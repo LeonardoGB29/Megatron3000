80e735397300cbb8cc771b54689772772e6d3bfd
LeonardoGB29 <lgaonab@unsa.edu.pe>
2024-09-21 11:38:06 -0500

    generador de schema termiando, falta generar correctamente las instancias verificando el tipo de dato

--- dataBase.cpp
+++ dataBase.cpp
@@ -10,6 +10,26 @@ std::string removeChars(const std::string& str, char charToRemove) {
     return result;
 }
 
+std::string replaceWithSymbol(const std::string& input, char symbol) { // symbol =  ","
+    std::string result;
+    bool insideQuotes = false;
+
+    for (char c : input) {
+        if (c == '"') {
+            insideQuotes = !insideQuotes;
+            result += c;
+        }
+        else if (c == ',' && !insideQuotes) {  //a,b,"a,b"
+            result += symbol;
+        }
+        else {
+            result += c;
+        }
+    }
+
+    return result;
+}
+
 void dataBase::parseAndExecuteQuery(const std::string& query) {
     std::istringstream iss(query);
     std::string command;
@@ -26,7 +46,11 @@ void dataBase::parseAndExecuteQuery(const std::string& query) {
     }
 }
 
+// CREATE TABLE employee(name VARCHAR, age INT);
+
 void dataBase::createTable(const std::string& query) {
+
+    //std::cout << query << std::endl;
     std::istringstream iss(query);
     std::string command, tableKeyword, tableName;
     iss >> command >> tableKeyword >> tableName;
@@ -45,6 +69,7 @@ void dataBase::createTable(const std::string& query) {
         while (std::getline(iss, columnPart, ',')) {
             columnPart = removeChars(columnPart, '(');
             columnPart = removeChars(columnPart, ')');
+            columnPart = removeChars(columnPart, ';');
 
             std::istringstream columnStream(columnPart);
             std::string columnName, columnType;
@@ -62,7 +87,7 @@ void dataBase::createTable(const std::string& query) {
         std::cout << "Esquema creado correctamente en schema.txt" << std::endl;
     }
     else {
-        std::cout << "Comando incorrecto: se esperaba 'CREATE TABLE'." << std::endl;
+        std::cout << "Se esperaba 'CREATE TABLE'" << std::endl;
     }
 }
 
@@ -78,4 +103,188 @@ void dataBase::select(const std::string& query) {
     else {
         std::cout << "Formato de SELECT inválido." << std::endl;
     }
+}
+
+void dataBase::displayRelations() {
+    std::ifstream schemaFile("schema.txt");
+    std::string line;
+
+    if (!schemaFile.is_open()) {
+        std::cerr << "No existe schema.txt. Por favor, crea un esquema en MEGATRON." << std::endl;
+        return;
+    }
+
+    std::cout << "\nTienes estas relaciones:" << std::endl;
+
+    while (std::getline(schemaFile, line)) {
+        std::istringstream iss(line);
+        std::string relationName;
+        std::getline(iss, relationName, '#');
+        std::cout << "    -> " << relationName << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    schemaFile.close();
+}
+
+std::string dataBase::selectRelation() {
+
+    std::ifstream schemaFile("schema.txt");
+    std::string line, relationName;
+    bool found = false;
+    std::cout << "Escriba una relación: ";
+    std::cin >> relationName;
+
+    while (std::getline(schemaFile, line)) {
+        if (line.find(relationName) == 0) {
+            found = true;
+            break;
+        }
+    }
+
+    schemaFile.close();
+    return relationName;
+}
+
+bool dataBase::validateInstance(const std::string& schemaLine, const std::string& instance) { // en este punto la instancia ya esta generada, tengo que comprobar que 
+    
+    std::istringstream schemaStream(schemaLine);
+    std::istringstream instanceStream(instance);
+    std::string schemaPart, instanceValue;
+        
+    std::getline(schemaStream, schemaPart, '#');
+
+    while (std::getline(schemaStream, schemaPart, '#') && std::getline(instanceStream, instanceValue, '#')) {
+
+        std::getline(schemaStream, schemaPart, '#');
+
+        if (schemaPart == "INT") {
+            for (char c : instanceValue) {
+                if (c < '0' || c > '9') return false;
+            }
+        }
+
+        else if (schemaPart == "FLOAT") {
+
+            bool puntoEncontrado = false;
+
+            for (char c : instanceValue) {
+                if (c == '.') {
+                    if (puntoEncontrado) return false;
+                    puntoEncontrado = true;
+                }
+                else if (c < '0' || c > '9') return false;
+            }
+        }
+
+        else if (schemaPart == "CHAR") {
+            if (instanceValue.length() != 1)
+                return false;
+        }
+
+        else if (schemaPart == "VARCHAR") {
+            if (instanceValue.empty()) 
+                return false;
+        }
+
+        else if (schemaPart == "BOOL") {
+            if (!(instanceValue == "0" || instanceValue == "1" ||
+                instanceValue == "true" || instanceValue == "false" ||
+                instanceValue == "TRUE" || instanceValue == "FALSE")) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void dataBase::uploadInstances(const std::string& data, const char& symbol) {
+    std::ifstream schemaFile("schema.txt");
+    std::string schemaLine, instanceFile, relationName;
+
+    displayRelations();
+    relationName = selectRelation();
+
+
+    while (std::getline(schemaFile, schemaLine)) {
+        if (schemaLine.find(relationName) == 0) {
+            break;
+        }
+    }
+    schemaFile.close();
+
+    std::ifstream instancesFile(data);
+    std::string instance;
+
+    relationName += ".txt";
+
+    std::ofstream outputFile(relationName, std::ios::app);
+
+    while (std::getline(instancesFile, instance)) {
+
+        instance = replaceWithSymbol(instance, '#');
+
+        if (validateInstance(schemaLine, instance)) {
+            outputFile << instance << std::endl;
+            std::cout << "Instancia válida: " << instance << std::endl;
+        }
+        else {
+            std::cout << "Instancia inválida: " << instance << std::endl;
+        }
+    }
+    instancesFile.close();
+    outputFile.close();
+}
+
+
+
+void dataBase::showMenu() {
+    int option;
+    do {
+        std::cout << "\n########### MENU PRINCIPAL ###########" << std::endl;
+        std::cout << "1. Entrar el MEGATRON3000" << std::endl;
+        std::cout << "2. Generar instancias" << std::endl;
+        std::cout << "3. Salir\n" << std::endl;
+        std::cout << "Seleccione una opción: ";
+        std::cin >> option;
+        std::cout << std::endl;
+
+        switch (option) {
+        case 1: {
+
+            std::string query;
+            std::cout << "% MEGATRON3000" << std::endl << "    Welcome to MEGATRON 3000!" << std::endl;
+
+            do {
+                std::cout << "& ";
+                std::getline(std::cin, query);
+                if (query.empty()) continue;
+                parseAndExecuteQuery(query);
+            } while (query != "quit");
+
+            break;
+        }
+        case 2: {
+
+            std::string name;
+            char symbol;
+            std::cout << "Ingrese el nombre del archivo (con extensión): ";
+            std::cin >> name;
+            std::cout << std::endl <<"Con que caracter esta separado? ";
+            std::cin >> symbol;
+            std::cout << std::endl;
+            //error cuando se mande otro char que no sea con el que se separa 
+            uploadInstances(name, symbol);
+            break;
+        }
+        case 3:
+            std::cout << "Saliendo del programa..." << std::endl;
+            break;
+        default:
+            std::cout << "Opción no válida, intente de nuevo." << std::endl;
+        }
+
+    } while (option != 3);
 }
--- dataBase.h
+++ dataBase.h
@@ -7,6 +7,11 @@
 
 class dataBase {
 public:
+    void showMenu();
+    void displayRelations();
+    std::string selectRelation();
+    bool validateInstance(const std::string& schemaLine, const std::string& instance);
+    void uploadInstances(const std::string& data, const char& symbol);
     void parseAndExecuteQuery(const std::string& query);
     void createTable(const std::string& query);
     void select(const std::string& query);
